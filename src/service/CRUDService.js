280b4902fa801b5a0ca20c578df47dfe7d760eeb
Juan Camilo <61602045+juanmiloz@users.noreply.github.com>
2022-12-19 23:48:30 -0500

    add CRUDService

---
+++ src/service/CRUDService.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+export const METADATA = 'metadata';
+export const RULES = 'rules'
+export const EVALUATE = 'rules/eval'
+
+export class CRUDService{
+
+    static baseUrl = 'http://localhost:8080/';
+
+    
+    static getExtraInfo(serviceRoute, itemId) {
+
+        const url = this.baseUrl + serviceRoute + '/' + itemId
+        
+        return axios.get(
+            url
+        ).then(res => res.data);
+    }  
+
+    static get(serviceRoute) {
+
+        const url = this.baseUrl + serviceRoute + '/' + itemId
+        
+        return axios.get(
+            url
+        ).then(res => res.data);
+    }  
+
+    static post(newItem, serviceRoute) {
+
+
+        const url = this.baseUrl + serviceRoute;
+
+
+        return axios.post(url, newItem)
+            .then(res => res.data)
+            .catch(function(error) {
+                if (error.response) {
+                    // Request made and server responded
+                    alert("ERROR " + error.response.data.code + "\n" + error.response.data.message);
+
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message);
+                }
+
+            });
+
+    }
+
+}
